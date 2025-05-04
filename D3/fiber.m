d381d6e642881acfa3738f024ef6058d5a577053
sagouzal-23 <said.agouzal@telecom-paris.fr>
2025-05-04 23:45:21 +0200

    Updating fiber.m code, BER Vs Power part WIP

--- D3/fiber.m
+++ D3/fiber.m
@@ -1,25 +1,20 @@
-function S_out = fiber(S_in,Nb_symbols,distance,Ts)
+function S_out = fiber(S_in,distance,Ts,sur_echantionnage)
+    N = length(S_in);
     sur_sig = [];
     for i = 1:1:N
-        for j = 1:1:10
-            sur_sig = [sur_sig,S_in(i)]
+        for j = 1:1:sur_echantionnage
+            sur_sig = [sur_sig,S_in(i)];
         end
     end
-    N = length(sur_sig)
-    beta_2 = 0 ;
-    beta_3 = 0 ;
-    Fs = 10/Ts  ;
-    f_0     = 193e12   ;
-    W0      = 2*pi*f_0 ;
-    window  = blackman(N);
-    signal  = sur_sig(:).*window;
+    N = length(sur_sig);
+    beta2 = -23.91e-24 ;
+    beta3 = 0.1e-36 ;
+    Fs = sur_echantionnage/Ts;
+    signal  = sur_sig;
     DFT     = fft(signal);
-    pas_Fs  = round(Fs/(N));
-    for i=1:1:N
-        w2 = (2*pi*Fs*i)^2;
-        w3 = (2*pi*Fs*i)^3;
-        disp = exp(-distance*(1i*(beta_2*w2/2+beta_3*w3/6)));
-        sur_sig(i)=sur_sig(i)*disp;
-    end
-    S_out = ifft(sur_sig);
+    f = (-N/2:N/2-1)*(Fs/N);
+    omega = 2*pi*f;
+    H = exp(-1i * (beta2/2 * omega.^2 + beta3/6 * omega.^3) * distance);
+    DFT = fftshift(DFT);
+    S_out = real(ifft(ifftshift(DFT .* H)));
 end
